d2ba2b034988973189064a1924f6d6fd5779f022
ReygaFitra <regafitra12@gmail.com>
2025-05-18 18:56:42 +0700

    add prepared statement parameters value

---
+++ src/main/java/com/reyga/dev/enumeration/StatementParamEnum.java
@@ -0,0 +1,5 @@
+package com.reyga.dev.enumeration;
+
+public enum StatementParamEnum {
+    NO_GENERATED_KEYS, RETURN_GENERATED_KEYS;
+}
--- src/main/java/com/reyga/dev/repository/NativeQueryLibrary.java
+++ src/main/java/com/reyga/dev/repository/NativeQueryLibrary.java
@@ -1,5 +1,6 @@
 package com.reyga.dev.repository;
 
+import com.reyga.dev.enumeration.StatementParamEnum;
 import com.reyga.dev.util.ConnectionUtil;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -77,6 +78,27 @@ public class NativeQueryLibrary {
         }
     }
 
+    public PreparedStatement constructPreparedStatement(HikariDataSource dataSource, String sql, StatementParamEnum statementParamEnum) {
+        Connection connection;
+        try {
+            if (dataSource == null) {
+                connection = connectionUtil.getDataSource().getConnection();
+            } else {
+                connection = dataSource.getConnection();
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        try (connection) {
+            try (PreparedStatement statement = this.preparedStatementParameters(connection, sql, statementParamEnum)) {
+                return statement;
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
     public ResultSet constructResultSetExecQuery(PreparedStatement statement) {
         try (ResultSet resultSet = statement.executeQuery()) {
             return resultSet;
@@ -107,5 +129,13 @@ public class NativeQueryLibrary {
         } catch (SQLException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    private PreparedStatement preparedStatementParameters(Connection connection, String sql, StatementParamEnum statementParamEnum) throws SQLException {
+        return switch (statementParamEnum) {
+            case NO_GENERATED_KEYS -> connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
+            case RETURN_GENERATED_KEYS -> connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            default -> throw new IllegalArgumentException("Unsupported StatementParamEnum: " + statementParamEnum);
+        };
     }
 }
